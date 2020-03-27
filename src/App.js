f7f710a9f3aa99d1a3ed76d66c63b77ad765c605
morantaf <fall_moranta@live.fr>
2020-03-27 14:37:24 +0100

    Add the Route to /signup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import { Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import Footer from "./components/Footer";
+import SignupForm from "./components/SignupForm";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Route exact path="/login" component={LoginForm} />
+      <Route path="/signup" component={SignupForm} />
       <Footer />
     </div>
   );
