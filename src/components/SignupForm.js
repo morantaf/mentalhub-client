fb2328f648a0d45ac146345cf01aa967b68c4286
morantaf <fall_moranta@live.fr>
2020-03-27 14:37:47 +0100

    Create Signup Form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,58 +1,154 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
-import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { signup } from "../store/actions/userActions";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles(theme => ({
-  root: {
+  paper: {
+    marginTop: theme.spacing(8),
     display: "flex",
-    flexWrap: "wrap",
-    "& > *": {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16)
-    }
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
   }
 }));
 
-export default function UserForm(props) {
+function SignupForm({ signup }) {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [practician, setPractician] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log("submit");
+    signup(email, password);
+  };
 
   return (
-    <Paper elevation={3}>
-      <div className={classes.root}>
-        <h1>Signup</h1>
-        <form onSubmit={props.onSubmit}>
-          <TextField
-            required
-            label="Email"
-            name="email"
-            value={props.values.email}
-            onChange={props.onChange}
-          />
-          <TextField
-            required
-            label="Username"
-            name="username"
-            value={props.values.username}
-            onChange={props.onChange}
-          />
-
-          <TextField
-            required
-            label="password"
-            name="password"
-            type="password"
-            value={props.values.password}
-            onChange={props.onChange}
-          />
-
-          <Button variant="contained" type="submit">
-            Submit
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    value={practician}
+                    onChange={e => setPractician(!practician)}
+                    color="primary"
+                  />
+                }
+                label="Sign up as a practician"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
           </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
         </form>
       </div>
-    </Paper>
+      <Box mt={5}></Box>
+    </Container>
   );
 }
+
+export default connect(null, { signup })(SignupForm);
