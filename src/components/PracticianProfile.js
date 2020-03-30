3e98bdf91aa4bf02c640afa0d3c4209f6d7a7108
morantaf <fall_moranta@live.fr>
2020-03-30 14:24:56 +0200

    Adding components to the Profile Pages

--- src/components/PracticianProfile.js
+++ src/components/PracticianProfile.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import request from "superagent";
 import { connect, useDispatch } from "react-redux";
-import { singlePractician } from "../store/actions/practicianAction";
 import { useSelector } from "react-redux";
-import InformationCard from "./InformationCard";
+import PresentationCard from "./PresentationCard";
 import PricesCard from "./PricesCard";
+import SpecializationsCard from "./SpecializationsCard";
+import EducationCard from "./EducationCard";
+import ContactCard from "./ContactCard";
+import { singlePractician } from "../store/actions/practicianAction";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -12,7 +15,8 @@ const baseUrl = "http://localhost:4000";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    flexDirection: "column"
   },
   paper: {
     height: 140,
@@ -22,8 +26,21 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2)
   },
   card: {
-    marginBottom: 25,
-    marginTop: 25
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: "20%",
+    width: 500
+  },
+  cardIcon: {
+    alignSelf: "center",
+    marginRight: 10
+  },
+  background: {
+    backgroundColor: "azure"
+  },
+  top: {
+    height: 300,
+    backgroundColor: "lightsteelblue"
   }
 }));
 
@@ -42,14 +59,26 @@ function PracticianProfile(props) {
     fetchPractician(id);
   }, []);
   return (
-    <div>
+    <div className={classes.background}>
+      <div className={classes.top} />
       {practician ? (
         <Grid container className={classes.root} spacing={2}>
-          <InformationCard
+          <PresentationCard
             description={practician.presentation}
+            languages={practician.user.languages}
             style={classes}
           />
           <PricesCard pricesList={practician.prices} style={classes} />
+          <SpecializationsCard
+            specializationsList={practician.specializations}
+            style={classes}
+          />
+          <EducationCard education={practician.education} style={classes} />
+          <ContactCard
+            style={classes}
+            email={practician.user.email}
+            phoneNumber={practician.user.whatsAppNumber}
+          />
         </Grid>
       ) : (
         <p>loading...</p>
