c9345d7d0129d73d15f2154ea44e4e3ba93c2a71
morantaf <fall_moranta@live.fr>
2020-04-02 17:35:58 +0200

    Clean comment Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-//https://dz9yg0snnohlc.cloudfront.net/im-looking-for-psychotherapy-near-me-what-are-the-best-options-1-new.jpg
 
 export default function Homepage() {
   return (
