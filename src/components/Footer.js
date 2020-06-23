cbdcb1df98ed4dd53e932e5bddcb58a3a081fa42
morantaf <fall_moranta@live.fr>
2020-06-23 15:39:56 +0200

    Changing the color to the secondary color from the theme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,31 +4,31 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   toolbar: {
     minHeight: 128,
     alignItems: "flex-start",
     paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(2)
+    paddingBottom: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
-    alignSelf: "flex-end"
-  }
+    alignSelf: "flex-end",
+  },
 }));
 
-export default function ProminentAppBar() {
+export default function Footer() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar color="secondary" position="static">
         <Toolbar className={classes.toolbar}>
           <Typography className={classes.title} variant="h5" noWrap>
             Footer
