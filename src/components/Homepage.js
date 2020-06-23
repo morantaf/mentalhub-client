cb6aac652e0ebcdfc7582d923436374b80b13a49
morantaf <fall_moranta@live.fr>
2020-06-23 15:42:50 +0200

    Adding the list of practicians to the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import PracticiansList from "./PracticiansList";
 
 export default function Homepage() {
   return (
     <div>
       <Header />
+      <PracticiansList />
     </div>
   );
 }
