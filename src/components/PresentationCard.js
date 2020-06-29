a797e30fc8d1e0f5c29a43383ac04e0dbbf36235
morantaf <fall_moranta@live.fr>
2020-06-29 16:19:20 +0200

    Correcting typo

--- src/components/PresentationCard.js
+++ src/components/PresentationCard.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import request from "superagent";
-s;
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
